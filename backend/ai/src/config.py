c89479e086f9ac816eda666b9dae42e581b2a75b
Harsh Bhatt <harsh.aby.007@gmail.com>
2025-03-02 19:11:01 +0530

    final changes

--- backend/ai/src/config.py
+++ backend/ai/src/config.py
@@ -6,9 +6,6 @@ class _Config:
     debug: bool
     ai_config: dict
 
-    oxylabs_username: str
-    oxylabs_password: str
-
     near_rpc_url: str
     near_account_id: str
     near_account_private_key: str
@@ -25,8 +22,6 @@ class _Config:
         with open(ai_config_path) as ai_config_file:
             self.ai_config = yaml.safe_load(ai_config_file)
 
-        self.oxylabs_username = os.getenv("OXYLABS_USERNAME")
-        self.oxylabs_password = os.getenv("OXYLABS_PASSWORD")
 
         self.near_rpc_url = os.getenv("NEAR_RPC_URL")
         self.near_account_id = os.getenv("NEAR_ACCOUNT_ID")
